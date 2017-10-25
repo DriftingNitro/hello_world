4d17d125686516f94e35b9698ce6a84ad1bcb3ce
John Colasante <Classafieduser@gmail.com>
2017-10-25 12:32:37 -0400

    Added a line to put in your name

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 // a simple program to practice common Github commands 
-
+import java.util.Scanner;  
 public class hello {
 
     public static void main(String [] args) {
@@ -11,6 +11,12 @@ public class hello {
 
         System.out.println("Hello Class!");
 
+        Scanner sc=new Scanner(System.in);  
+            
+        System.out.println("Enter your name");  
+        String name=sc.next();  
+        System.out.println("Name:"+name);  
+        sc.close();  
 
 
     }
